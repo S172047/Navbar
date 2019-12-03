bc470b50591a8c0f1b9f0d26e7d1efea32886556
Sanna Svensson <s172047@student.hb.se>
2019-12-03 14:29:15 +0100

    Updates

---
+++ precache-manifest.54da62be69f14e8a691dab004ed460c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86198da1b9d219e90cd30eeac3362623",
+    "url": "/Navbar/index.html"
+  },
+  {
+    "revision": "267da7af701fb84d1de8",
+    "url": "/Navbar/static/css/2.247797c6.chunk.css"
+  },
+  {
+    "revision": "8bee970bf502c9362f9a",
+    "url": "/Navbar/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "267da7af701fb84d1de8",
+    "url": "/Navbar/static/js/2.19087065.chunk.js"
+  },
+  {
+    "revision": "8bee970bf502c9362f9a",
+    "url": "/Navbar/static/js/main.39e79533.chunk.js"
+  },
+  {
+    "revision": "ad54ecc6e23b280295df",
+    "url": "/Navbar/static/js/runtime-main.fefa4d9e.js"
+  }
+]);
