e38efbf2822884dbbedf47cae3a53f091cf7c2f7
Sanna Svensson <s172047@student.hb.se>
2019-12-04 12:05:47 +0100

    changes

--- src/component/GlacierSea.jsx
+++ src/component/GlacierSea.jsx
@@ -12,15 +12,50 @@ import {
 
 class GlacierSea extends Component {
   render() {
+
+      let GlacierSizedata = this.props.Glacierdata.filter(x => x.Year > 1945);
+      let SeaLeveldata = this.props.SeaLeveldata.filter(x => parseInt (x.Time) >1945);
+
+      let series = [
+        {
+          name: "Glacier Data",
+          data: [],
+          color: "green"
+        },
+
+        {
+          name: "Sea Level Data",
+          data: [],
+          color: "blue"
+        }
+      ];
+
+      GlacierSizedata.map(data => series[1].data.push ({
+        Year: data.Year,
+        value: data ["Mean cumulative mass balance"]
+      })
+      );
+
+      SeaLeveldata.map(data => series [0].data. push ({
+        Year: parseInt(data.Time),
+        value: data.GMSL
+      })
+      );
+
+      console.log(series);
+
+
+
+
     return (
       <React.Fragment>
         <p style={{ fontSize: "24px", fontWeight: "bold" }}>
           Korrelation mellan minskning av glaciärer och havsytans ökning
         </p>
+
         <LineChart
           width={1400}
           height={500}
-          data={this.props.CO2data}
           margin={{
             top: 10,
             right: 30,
@@ -30,23 +65,39 @@ class GlacierSea extends Component {
         >
           <CartesianGrid strokeDasharray="3 3"></CartesianGrid>
 
-          <XAxis dataKey="Year"></XAxis>
-          <YAxis />
+          <XAxis dataKey="Year" type="Year"></XAxis>
+          <YAxis dataKey="value" />
           <Tooltip />
           <Legend verticalAlign="top" />
+
+          {series.map (s => (
+
           <Line
             type="monotone"
-            dataKey="Total"
-            stroke="#8884d8"
+            dataKey="value"
+            data={s.data}
+            name="Glacier"
+            key={s.name}
+            stroke={s.color}
             activeDot={{ r: 8 }}
           />
-          <Line type="monotone" dataKey="Mean cumulative mass balance" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="Time" stroke="orange" />
-         
-        </LineChart>
-      </React.Fragment>
-    );
-  }
-}
 
-export default GlacierSea;
+          <Line
+          type="monotone"
+            dataKey="value"
+            data={s.data}
+            name="Sea Level"
+            key={s.name}
+            stroke={s.color}
+
+          />
+
+          ))}
+
+          </LineChart>
+          </React.Fragment>
+        );
+      }
+    }
+    
+    export default GlacierSea;
